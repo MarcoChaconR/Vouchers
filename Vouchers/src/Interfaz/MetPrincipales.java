466e4805efae3a7d1418f174dba1f2c7f822f69f
mchaconr <mchaconr@DELL50080316.grupoins.local>
2019-03-27 20:07:19 -0600

    Metodos de consulta de archivos

--- Vouchers/src/Interfaz/MetPrincipales.java
+++ Vouchers/src/Interfaz/MetPrincipales.java
@@ -6,18 +6,44 @@
 package Interfaz;
 
 import java.awt.Component;
+import java.io.File;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
  * @author mchaconr
  */
 public class MetPrincipales {
-    public static void VouchersColones(){
-        JOptionPane.showMessageDialog(PrincipalForm(), "Holis");
+
+    public static File VouchersColones(Component parent, String moneda) {
+        String optiones[] = {"Continuar", "Cancelar"};
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+        FileNameExtensionFilter filtro = new FileNameExtensionFilter("Archivos de texto", "txt");
+        fileChooser.setFileFilter(filtro);
+
+        int result = fileChooser.showOpenDialog(parent);
+
+        if (result != JFileChooser.CANCEL_OPTION) {
+
+            File fileName = fileChooser.getSelectedFile();
+
+            if ((fileName == null) || (fileName.getName().equals(""))) {
+                JOptionPane.showMessageDialog(parent, "...");
+            } else {
+                if (0 == JOptionPane.showOptionDialog(parent, "El archivo a procesar es: " + fileName.getName(), "Abrir Vouchers Credomatic "
+                        + moneda, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, optiones, null)) {
+                    return fileName;
+                } else {
+                    JOptionPane.showMessageDialog(parent, "No se cargara ningún Archivo!", "Mensage!", JOptionPane.WARNING_MESSAGE);
+                }
+
+            }
+        }
+        return null;
     }
 
-    private static Component PrincipalForm() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
